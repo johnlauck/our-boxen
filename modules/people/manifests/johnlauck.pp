f9c42102d4f0eac82e01e7fec0d6b4ec26e4d7e8
John Lauck <john@recaffeinated.com>
2014-12-01 12:18:11 -0500

    Remove pathfinder, need to fix version

--- modules/people/manifests/johnlauck.pp
+++ modules/people/manifests/johnlauck.pp
@@ -20,6 +20,21 @@ class people::johnlauck {
     magnification_size => 84
   }
 
+  # $home = "/Users/${::boxen_user}"
+  # $dotfiles_dir = "${boxen::config::srcdir}/dotfiles"
+  #
+  # repository { $dotfiles_dir:
+  #   source => "${::github_user}/dotfiles"
+  # }
+  #
+  # exec { "install dotfiles":
+  #   cwd      => $dotfiles_dir,
+  #   command  => "./install.sh",
+  #   provider => shell,
+  #   creates  => "${home}/.zshrc",
+  #   require  => Repository[$dotfiles_dir]
+  # }
+
   include people::johnlauck::applications
   include people::johnlauck::dev
   include people::johnlauck::shell
