5c7e64be2ceb9ff45ea10f738088c29247dd8f73
John Lauck <john@recaffeinated.com>
2014-12-09 03:00:44 -0500

    Add ruby_gem bundler, and ansible test

--- modules/people/manifests/johnlauck/dev.pp
+++ modules/people/manifests/johnlauck/dev.pp
@@ -11,10 +11,21 @@ class people::johnlauck::dev {
     ensure => present,
   }
 
+  # package { "ansible":
+  #   ensure => present,
+  # }
+
   # http://support.apple.com/kb/DL1572
   package { 'JavaOsx':
     source  => 'http://support.apple.com/downloads/DL1572/en_US/JavaForOSX2014-001.dmg',
     provider => pkgdmg;
   }
+
+  ruby_gem { 'bundler for all rubies':
+    gem          => 'bundler',
+    version      => '~> 1.0',
+    ruby_version => '*',
+  }
+
 
 }
