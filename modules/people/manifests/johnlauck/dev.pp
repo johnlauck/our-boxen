8ae780c85a4ef98723d956e94854cf4d980f505c
John Lauck <john@recaffeinated.com>
2015-01-19 11:26:55 -0500

    Add brew packages

--- modules/people/manifests/johnlauck/dev.pp
+++ modules/people/manifests/johnlauck/dev.pp
@@ -22,6 +22,18 @@ class people::johnlauck::dev {
     ensure => present,
   }
 
+  package { "libxml2":
+    ensure => present,
+  }
+
+  package { "v8":
+    ensure => present,
+  }
+
+  package { "aws-elasticbeanstalk":
+    ensure => present,
+  }
+
   # http://support.apple.com/kb/DL1572
   package { 'JavaOsx':
     source  => 'http://support.apple.com/downloads/DL1572/en_US/JavaForOSX2014-001.dmg',
